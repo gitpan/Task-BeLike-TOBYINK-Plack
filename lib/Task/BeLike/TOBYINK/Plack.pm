8111c4def499fa7547d44bd0a3f8458a7e4bc21b
Toby Inkster <tobyink@cpan.org>
2012-05-29 22:23:26 +0000

    Import of TOBYINK/Task-BeLike-TOBYINK-Plack-2012.150 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-TOBYINK-Plack
    gitpan-cpan-version:      2012.150
    gitpan-cpan-path:         TOBYINK/Task-BeLike-TOBYINK-Plack-2012.150.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/TOBYINK/Plack.pm
@@ -0,0 +1,53 @@
+package Task::BeLike::TOBYINK::Plack;
+
+use 5.010;
+
+BEGIN {
+	$Task::BeLike::TOBYINK::Plack::AUTHORITY = 'cpan:TOBYINK';
+	$Task::BeLike::TOBYINK::Plack::VERSION   = '2012.150';
+}
+
+__PACKAGE__
+__END__
+
+=head1 NAME
+
+Task::BeLike::TOBYINK::Plack - be like TOBYINK and use these Plack stuffz
+
+=head1 DESCRIPTION
+
+Task installs the following modules:
+
+=over
+
+=item * Gepok
+
+=item * Plack
+
+=item * RDF::ACL
+
+=item * Web::ID
+
+=back
+
+=head1 SEE ALSO
+
+L<Acme::About::TOBYINK>,
+L<Task::BeLike::TOBYINK>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2012 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
